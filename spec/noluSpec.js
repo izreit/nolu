7b87eb53e5252ee208b6404187e86b95b5c420e7
Kaname Kizu <kaname.kizz@gmail.com>
2017-03-11 19:30:22 +0900

    test: introduce Jasmine

---
+++ spec/noluSpec.js
@@ -0,0 +1,9 @@
+var nolu = require("../lib/index");
+
+describe("nolu", function () {
+  describe("-e", function () {
+    it("executes specified code", function () {
+      expect(1).toBe(2);
+    });
+  });
+});
